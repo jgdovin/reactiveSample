349b75ab745a2633b436bd36cba5319b0640806b
Joshua Gdovin <joshuag@focusschoolsoftware.com>
2015-07-13 14:06:31 -0400

    basic app

---
+++ reactiveSample.js
@@ -0,0 +1,27 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.onCreated(function(){
+    this.currentActive = new ReactiveVar(0);
+  })
+
+  Template.hello.helpers({
+    tasks: function () {
+      return [
+        {id: 1, title: 'get stuff done', completed: false},
+        {id: 2, title: 'buy milk', completed: false},
+        {id: 3, title: 'have fun', completed: false}
+      ]
+    },
+    active: function() {
+      return this.id === Template.instance().currentActive.get();
+    }
+  });
+
+  Template.hello.events({
+    'click span.edit': function (event, template) {
+      template.currentActive.set(this.id);
+    }
+  });
+}
